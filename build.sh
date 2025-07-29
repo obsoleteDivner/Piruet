e3bd2f2c16fd8d0bf3c18eb253e067b98d065266
Anatolii Voloshyn <anatolii.voloshyn@marmend.com>
2025-07-29 03:41:03 +0300

    temporary rmoved ecto

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
-#!/usr/bin/env bash
 set -o errexit
+
+if ! command -v yt-dlp &> /dev/null; then
+  echo "Installing yt-dlp..."
+  sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+  sudo chmod a+rx /usr/local/bin/yt-dlp
+fi
+
+if ! command -v streamlink &> /dev/null; then
+  echo "Installing streamlink..."
+  sudo apt-get update
+  sudo apt-get install -y streamlink
+fi
+
 mix clean
 rm -rf _build
 mix deps.get --only prod
